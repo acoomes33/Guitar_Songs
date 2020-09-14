bd817436f848bba4e9328412f6b8fe5cb7ed08b8
acoomes33 <github email address>
2020-09-14 17:10:03 -0500

    created songs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200914213623) do
+ActiveRecord::Schema.define(version: 20200914214228) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string  "name"
+    t.string  "artist"
+    t.string  "genre"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
