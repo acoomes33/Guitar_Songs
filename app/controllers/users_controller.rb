6f502f5f503caca127f76fbd87bbd2b671bb965b
acoomes33 <github email address>
2020-09-15 16:36:29 -0500

    added views files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
     
+    get "/signup" do
+        erb :signup
+    end
+
+    post "/signup" do
+        if params[:username] == "" || params[:password] == ""
+            redirect "/failure"
+        else
+            @user = User.create(:username => params[:username], :password => params[:password])
+    end 
+
+
+
 end 
