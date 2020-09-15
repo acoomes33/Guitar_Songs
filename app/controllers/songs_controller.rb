58c3d86e515efdb9245e785c4a852a3f9d5ebb7c
acoomes33 <github email address>
2020-09-15 18:24:34 -0500

    filled in seeds

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,32 @@
 class SongsController < ApplicationController
-    
+
+    get "/songs" do
+        @songs = Song.all 
+        erb :"/songs/index"
+    end 
+
+    get "/songs/new" do
+        erb :"/songs/new"
+    end 
+        
+    post "/songs" do
+    end 
+
+    get "/songs/:id" do
+        @song = Song.find_by_id(params[:id])
+        erb :"/songs/show"
+    end 
+
+    get "/songs/:id/edit" do
+        erb :"/songs/edit"
+    end 
+
+    patch "/songs/:id" do 
+        
+    end 
+
+    delete "/songs/:id" do 
+
+    end 
+
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,29 @@ class UsersController < ApplicationController
             redirect "/failure"
         else
             @user = User.create(:username => params[:username], :password => params[:password])
+        end 
     end 
 
+    get "/users" do
+    end 
 
+    get "/users/new" do
+        erb :
+    end 
+        
+    post "/users" do
+    end 
+
+    get "/users/:id" do
+    end 
+
+    get "/users/:id/edit" do
+    end 
+
+    patch "/users/:id" do 
+    end 
+
+    delete "/users/:id" do 
+    end 
 
 end 
