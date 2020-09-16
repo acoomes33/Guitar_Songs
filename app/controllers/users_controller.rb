9c1fa13f2be3e9cfb0a9eea47c80f64545c2f7c9
acoomes33 <github email address>
2020-09-16 18:50:56 -0500

    updated views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,26 +28,4 @@ class UsersController < ApplicationController
         end
     end 
 
-    get "/users" do
-    end 
-
-    get "/users/new" do
-        
-    end 
-        
-    post "/users" do
-    end 
-
-    get "/users/:id" do
-    end 
-
-    get "/users/:id/edit" do
-    end 
-
-    patch "/users/:id" do 
-    end 
-
-    delete "/users/:id" do 
-    end 
-
 end 
